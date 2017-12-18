e2b8fb4cc5aaed3302b695c5b204e245803d0965
Vincent Huynh <vincenthuynh@Vincents-MacBook-Pro.local>
2017-12-18 15:29:04 -0600

    final changes

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendList = [
+  {
+    name: 'Vincent Huynh',
+    profilePic: '###',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+ 
+]
+
+module.exports = friendList;
